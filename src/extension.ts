5aff7b42d31b11855e44e2ccd4224936b1b3a097
DeveloperEstiven <hundsomeluke@gmail.com>
2024-07-31 00:38:08 +0300

    feature/initial-version

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,16 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { COMMANDS } from "./commands";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const { registerCommand } = vscode.commands;
+
 export function activate(context: vscode.ExtensionContext) {
+  vscode.window.showInformationMessage("Turbo System Debug is Activated");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "turbo-system-debug" is now active!');
+  const logDisposable = registerCommand("turbo-system-debug.log", COMMANDS.log);
+  context.subscriptions.push(logDisposable);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('turbo-system-debug.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from turbo-system-debug!');
-	});
-
-	context.subscriptions.push(disposable);
+  const removeAllDisposable = registerCommand("turbo-system-debug.removeAll", COMMANDS.removeAll);
+  context.subscriptions.push(removeAllDisposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
